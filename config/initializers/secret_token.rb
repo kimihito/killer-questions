7073057ccccf85c1efa02dbc18a35945297180ae
kimihito <tatsurotamashiro@gmail.com>
2013-03-07 14:56:40 +0900

    initial commit (use twitter-bootstrap)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KillerQuestions::Application.config.secret_token = 'abef41f14a18ca40b291258af303437931324ff3c7fa546500548507e03846abb68529eee648b4f77231c10b935d653ea8efa7bc0ea403242cfa0d8487b2d49a'
